85922f8766d49b63a257d3d211002050bd0e8bbc
CNTLSN <aless.contini@gmail.com>
2016-08-22 11:33:54 +0200

    add rephone hello world sketch

---
+++ utilities/RePhone_Hello_World/RePhone_Hello_World.ino
@@ -0,0 +1,29 @@
+﻿/*
+  Rephone Kit Hello World
+  
+  Hello, this is a very simple sketch to test the
+  communication between your computer and the
+  Rephone's main module, Xadow GSM+BLE.
+  
+  INSTRUCTIONS:
+  Select "Debug Port" from Tools > Port menu.
+  After successful upload, change the port to "Modem Port".
+  Open Serial Monitor and set baud rate at 115200.
+  
+  HARDWARE:
+  - Xadow GSM+BLE
+  
+  Alessandro Contini for http://opencare.cc/
+  August 2016
+*/
+
+void setup() {
+  // start serial communication
+  Serial.begin(115200);
+}
+
+void loop() {
+  // print Hello World! on the Serial Monitor
+  Serial.println("Hello World!");
+  delay(100);
+}
