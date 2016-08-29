33f8ba7e66bcc1bb204677b73f6f3c37e8335e8d
CNTLSN <aless.contini@gmail.com>
2016-08-29 13:33:13 +0200

    add sketch that pushes battery state to IFTTT via http requests

---
+++ utilities/Battery_IFTTT/L_https.h
@@ -0,0 +1,24 @@
+#ifndef _L_HTTPS_H
+#define _L_HTTPS_H
+
+#include "LTask.h"
+
+class LHttpsClass : public _LTaskClass 
+{
+public:
+LHttpsClass() {};
+
+public:
+
+void connect(char* url);
+void get_handle(void(*callback)(char *, unsigned long));
+void stop(void);
+
+private:
+int read_ok;
+};
+
+extern LHttpsClass https;
+
+
+#endif
---
+++ utilities/Battery_IFTTT/_https.h
@@ -0,0 +1,9 @@
+#ifndef __HTTPS_H
+#define __HTTPS_H
+
+boolean https_connect(void* user_data);
+boolean https_stop(void* user_data);
+boolean https_set_new_event_callback(void(*cb)(char *, unsigned long));
+
+
+#endif
