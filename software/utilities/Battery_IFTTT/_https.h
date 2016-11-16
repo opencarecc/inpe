6c09622d3176ea819544bcdac86642647706bd8f
CNTLSN <aless.contini@gmail.com>
2016-11-16 14:52:12 +0100

    remove battery ifttt sketch

--- software/utilities/Battery_IFTTT/_https.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HTTPS_H
-#define __HTTPS_H
-
-boolean https_connect(void* user_data);
-boolean https_stop(void* user_data);
-boolean https_set_new_event_callback(void(*cb)(char *, unsigned long));
-
-
-#endif
