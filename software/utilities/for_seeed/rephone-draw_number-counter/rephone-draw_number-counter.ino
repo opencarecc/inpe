b7e0bd6056c9d99f3dc6278d5210fbae01ec4fc8
CNTLSN <aless.contini@gmail.com>
2016-11-28 18:48:05 +0100

    add files for seeed

---
+++ software/utilities/for_seeed/rephone-draw_number-counter/rephone-draw_number-counter.ino
@@ -0,0 +1,20 @@
+#include <LDisplay.h>
+
+void setup() {
+  Lcd.init();
+  Lcd.font_init();
+  Lcd.back_light_level(50);
+  Lcd.screen_set(0xffff00);
+}
+
+void loop() {
+  Lcd.draw_fill_rectangle(30,30,130,130,0x0000f0);
+  for(int i;i<=100;i++){
+    Lcd.draw_number(70, 50, i, 0x0000f0, 0xffffff);
+    delay(500);
+    if(i==100){
+      i=0;
+    }
+  }
+//  Lcd.draw_updata();
+}
---
+++ software/utilities/for_seeed/rephone-sleep_library/sleep.cpp
@@ -0,0 +1,53 @@
+#include "Arduino.h"
+
+#include "LDisplay.h"
+#include "LGTouch.h"
+
+#include "LTask.h"
+#include "vmpwr.h"
+
+#include "sleep.h"
+
+char EVENT = 0;
+int X = 0;
+int Y = 0;
+
+unsigned long previousMillis = 0;
+
+// set here (in milliseconds) the time interval to wait before going to sleep
+const long sleepInterval = 1000*10; 
+
+sleepClass::sleepClass(){}
+
+
+void sleepClass::sleep(){
+  // count time
+  unsigned long currentMillis = millis();
+  
+  // if current time count is equal or bigger than the interval of time to wait before going to sleep
+  if(currentMillis-previousMillis>=sleepInterval){
+    // set backlight low
+//    Lcd.back_light_level(1);
+
+    LTask.remoteCall((remote_call_ptr)vm_pwr_lcd_sleep_in, NULL);
+    
+    // save current time count to check for the time interval again later
+    previousMillis = currentMillis;
+  }
+  
+  // if there is a touch event
+  if(Tp.Event_available()){
+    Tp.Get_event_xy(&EVENT, &X, &Y);
+    if (EVENT>0 && EVENT!=6 && X>=0 && Y>=0){
+      // set backlight high
+//      Lcd.back_light_level(50);
+
+      LTask.remoteCall((remote_call_ptr)vm_pwr_lcd_sleep_out, NULL);
+
+      // save current count to wait for another whole time interval before going to sleep
+      previousMillis = currentMillis;
+    }
+  }
+}
+
+sleepClass sleep = sleepClass();
